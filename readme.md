295aa20d957ad889ad90cfb622148fd2e75a913a
idranme <idranme@outlook.com>
2024-07-20 00:47:30 +0800

    0.0.1

---
+++ readme.md
@@ -0,0 +1,5 @@
+# koishi-plugin-like-me
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-like-me?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-like-me)
+
+Like me
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Context, h, Schema } from 'koishi'
+
+export const name = 'like-me'
+
+export interface Config {
+  times: number
+}
+
+export const Config: Schema<Config> = Schema.object({
+  times: Schema.number().description('点赞次数').default(10)
+})
+
+export function apply(ctx: Context, cfg: Config) {
+  ctx.command('like-me', '为你点赞')
+    .alias('赞我')
+    .action(({ session }) => {
+      const like = '👍'.repeat(cfg.times)
+      return `${h.quote(session.messageId)}${like}<br/>给你赞了${cfg.times}下哦，记得回我~`
+    })
+}
